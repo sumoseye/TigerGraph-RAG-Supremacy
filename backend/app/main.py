fe47bcdc3b6bd6f8a6c1d8420c3a4b4542393924
sumoseye <livonad2006@gmail.com>
2026-05-17 23:14:50 +0530

    cors

--- backend/app/main.py
+++ backend/app/main.py
@@ -37,7 +37,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=settings.CORS_ORIGINS,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
